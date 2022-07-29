83090411ee5e4bf75e96e2a3f4fe09a5c56148c2
Daniel Plohmann (Saturn) <daniel.plohmann@mailbox.org>
2022-07-29 12:23:11 +0200

    allow full listing of all samples in malpedia-style folder

--- examples/cross_compare/generate_csv_from_malpedia.py
+++ examples/cross_compare/generate_csv_from_malpedia.py
@@ -6,7 +6,7 @@ import sys
 dump_file_pattern = re.compile("dump7?_0x[0-9a-fA-F]{8,16}")
 unpacked_file_pattern = re.compile("_unpacked(_x64)?$")
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print("A script to generate a cross-compare CSV based on Malpedia")
     print(f"usage: {sys.argv[0]} <malpedia_root> <family_a> <family_b> ... <family_n>")
     sys.exit()
@@ -19,7 +19,13 @@ for root, subdir, files in sorted(os.walk(malpedia_root)):
     path_split = relative_root.split(os.sep)
     family = path_split[0]
     version = path_split[1] if len(path_split) > 1 else ""
-    if family in target_families:
+    if len(target_families) > 0:
+        if family in target_families:
+            for filename in files:
+                if not (re.search(unpacked_file_pattern, filename) or re.search(dump_file_pattern, filename)):
+                    continue
+                print(f"{family},{version},{root + os.sep + filename}")
+    else:
         for filename in files:
             if not (re.search(unpacked_file_pattern, filename) or re.search(dump_file_pattern, filename)):
                 continue
